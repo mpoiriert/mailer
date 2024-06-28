80773755e9ebc1491b97bc1761a1d4fffc09bd15
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-06-28 13:49:24 -0400

    [Mailer] Use new draw/dependency-injection component

---
+++ DependencyInjection/MailerIntegration.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Draw\Component\Mailer\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\IntegrationTrait;
+use Draw\Component\DependencyInjection\Integration\PrependIntegrationInterface;
+use Draw\Component\Mailer\BodyRenderer\LocalizeBodyRenderer;
+use Draw\Component\Mailer\EmailComposer;
+use Draw\Component\Mailer\EmailWriter\DefaultFromEmailWriter;
+use Draw\Component\Mailer\EmailWriter\EmailWriterInterface;
+use Draw\Component\Mailer\EventListener\EmailCssInlinerListener;
+use Draw\Component\Mailer\EventListener\EmailSubjectFromHtmlTitleListener;
+use Pelago\Emogrifier\CssInliner;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Mime\Address;
+
+class MailerIntegration implements IntegrationInterface, PrependIntegrationInterface
+{
+    use IntegrationTrait;
+
+    public function getConfigSectionName(): string
+    {
+        return 'mailer';
+    }
+
+    public function load(array $config, PhpFileLoader $loader, ContainerBuilder $container): void
+    {
+        $this->registerClasses(
+            $loader,
+            $namespace = 'Draw\\Component\\Mailer\\',
+            \dirname(
+                (new \ReflectionClass(EmailComposer::class))->getFileName(),
+            ),
+        );
+
+        $container
+            ->getDefinition(LocalizeBodyRenderer::class)
+            ->setDecoratedService(
+                'twig.mime_body_renderer',
+                'draw.mailer.body_renderer.localize_body_renderer.inner'
+            )
+            ->setArgument('$bodyRenderer', new Reference('draw.mailer.body_renderer.localize_body_renderer.inner'));
+
+        $container
+            ->registerForAutoconfiguration(EmailWriterInterface::class)
+            ->addTag(EmailWriterInterface::class);
+
+        if (!$this->isConfigEnabled($container, $config['subject_from_html_title'])) {
+            $container->removeDefinition(EmailSubjectFromHtmlTitleListener::class);
+        }
+
+        if (!$this->isConfigEnabled($container, $config['css_inliner'])) {
+            $container->removeDefinition(EmailCssInlinerListener::class);
+        }
+
+        if (!$this->isConfigEnabled($container, $config['default_from'])) {
+            $container->removeDefinition(DefaultFromEmailWriter::class);
+        } else {
+            $container
+                ->getDefinition(DefaultFromEmailWriter::class)
+                ->setArgument(
+                    '$defaultFrom',
+                    (new Definition(Address::class))
+                        ->setArguments([$config['default_from']['email'], $config['default_from']['name'] ?? ''])
+                );
+        }
+
+        $this->renameDefinitions(
+            $container,
+            $namespace,
+            'draw.mailer.'
+        );
+    }
+
+    public function addConfiguration(ArrayNodeDefinition $node): void
+    {
+        $node
+              ->children()
+                ->arrayNode('subject_from_html_title')
+                    ->canBeEnabled()
+                ->end()
+                ->arrayNode('css_inliner')
+                    ->canBeEnabled()
+                    ->validate()
+                        ->ifTrue(fn ($value) => $value['enabled'] && !class_exists(CssInliner::class))
+                        ->thenInvalid('The css inliner is base on the [pelago/emogrifier] package. Install it if you want to enable this feature.')
+                    ->end()
+                ->end()
+                ->arrayNode('default_from')
+                    ->canBeEnabled()
+                    ->children()
+                        ->scalarNode('email')->isRequired()->end()
+                        ->scalarNode('name')->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    public function prepend(ContainerBuilder $container, array $config): void
+    {
+        $installationPath = \dirname((new \ReflectionClass(EmailWriterInterface::class))->getFileName(), 2);
+
+        $this->assertHasExtension($container, 'framework');
+
+        $container->prependExtensionConfig(
+            'framework',
+            [
+                'translator' => [
+                    'paths' => [
+                        'draw-mailer' => $installationPath.'/Resources/translations',
+                    ],
+                ],
+            ]
+        );
+
+        if ($container->hasExtension('twig')) {
+            $container->prependExtensionConfig(
+                'twig',
+                [
+                    'paths' => [
+                        $installationPath.'/Resources/views' => 'draw-mailer',
+                    ],
+                ]
+            );
+        }
+    }
+}
---
+++ Tests/DependencyInjection/MailerIntegrationTest.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace Draw\Component\Mailer\Tests\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\Test\IntegrationTestCase;
+use Draw\Component\DependencyInjection\Integration\Test\ServiceConfiguration;
+use Draw\Component\Mailer\BodyRenderer\LocalizeBodyRenderer;
+use Draw\Component\Mailer\Command\SendTestEmailCommand;
+use Draw\Component\Mailer\DependencyInjection\MailerIntegration;
+use Draw\Component\Mailer\EmailComposer;
+use Draw\Component\Mailer\EmailWriter\AddTemplateHeaderEmailWriter;
+use Draw\Component\Mailer\EmailWriter\DefaultFromEmailWriter;
+use Draw\Component\Mailer\EmailWriter\EmailWriterInterface;
+use Draw\Component\Mailer\EventListener\EmailComposerListener;
+use Draw\Component\Mailer\EventListener\EmailCssInlinerListener;
+use Draw\Component\Mailer\EventListener\EmailSubjectFromHtmlTitleListener;
+use Draw\Component\Mailer\Twig\TranslationExtension;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
+use Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * @property MailerIntegration $integration
+ */
+#[CoversClass(MailerIntegration::class)]
+class MailerIntegrationTest extends IntegrationTestCase
+{
+    public function createIntegration(): IntegrationInterface
+    {
+        return new MailerIntegration();
+    }
+
+    public function getConfigurationSectionName(): string
+    {
+        return 'mailer';
+    }
+
+    public function getDefaultConfiguration(): array
+    {
+        return [
+            'css_inliner' => [
+                'enabled' => false,
+            ],
+            'default_from' => [
+                'enabled' => false,
+            ],
+            'subject_from_html_title' => [
+                'enabled' => false,
+            ],
+        ];
+    }
+
+    public function testPrependNoFrameworkExtension(): void
+    {
+        static::expectExceptionMessage(
+            'You must have the extension [framework] available to configuration [draw_framework_extra.mailer]'
+        );
+
+        $this->integration->prepend(
+            new ContainerBuilder(),
+            []
+        );
+    }
+
+    public function testPrepend(): void
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->registerExtension(new FrameworkExtension());
+        $containerBuilder->registerExtension(new TwigExtension());
+
+        $this->integration->prepend(
+            $containerBuilder,
+            []
+        );
+
+        $installationPath = \dirname(
+            (new \ReflectionClass(EmailWriterInterface::class))->getFileName(),
+            2
+        );
+
+        static::assertContainerExtensionConfiguration(
+            $containerBuilder,
+            [
+                'framework' => [
+                    [
+                        'translator' => [
+                            'paths' => [
+                                'draw-mailer' => $installationPath.'/Resources/translations',
+                            ],
+                        ],
+                    ],
+                ],
+                'twig' => [
+                    [
+                        'paths' => [
+                            $installationPath.'/Resources/views' => 'draw-mailer',
+                        ],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public static function provideTestLoad(): iterable
+    {
+        $defaultServices = [
+            new ServiceConfiguration(
+                'draw.mailer.twig.translation_extension',
+                [
+                    TranslationExtension::class,
+                ]
+            ),
+            new ServiceConfiguration(
+                'draw.mailer.email_composer',
+                [
+                    EmailComposer::class,
+                ]
+            ),
+            new ServiceConfiguration(
+                'draw.mailer.body_renderer.localize_body_renderer',
+                [
+                    LocalizeBodyRenderer::class,
+                ]
+            ),
+            new ServiceConfiguration(
+                'draw.mailer.email_writer.add_template_header_email_writer',
+                [
+                    AddTemplateHeaderEmailWriter::class,
+                ]
+            ),
+            new ServiceConfiguration(
+                'draw.mailer.event_listener.email_composer_listener',
+                [
+                    EmailComposerListener::class,
+                ]
+            ),
+            new ServiceConfiguration(
+                'draw.mailer.command.send_test_email_command',
+                [
+                    SendTestEmailCommand::class,
+                ]
+            ),
+        ];
+
+        yield 'default' => [
+            [],
+            $defaultServices,
+        ];
+
+        yield 'subject_from_html_title' => [
+            [
+                ['subject_from_html_title' => true],
+            ],
+            array_merge(
+                $defaultServices,
+                [
+                    new ServiceConfiguration(
+                        'draw.mailer.event_listener.email_subject_from_html_title_listener',
+                        [
+                            EmailSubjectFromHtmlTitleListener::class,
+                        ]
+                    ),
+                ]
+            ),
+        ];
+
+        yield 'default_from' => [
+            [
+                [
+                    'default_from' => [
+                        'email' => 'test@example.com',
+                        'name' => 'Test Email',
+                    ],
+                ],
+            ],
+            array_merge(
+                $defaultServices,
+                [
+                    new ServiceConfiguration(
+                        'draw.mailer.email_writer.default_from_email_writer',
+                        [
+                            DefaultFromEmailWriter::class,
+                        ],
+                        function (Definition $definition): void {
+                            $defaultFromDefinition = $definition->getArgument('$defaultFrom');
+
+                            static::assertInstanceOf(
+                                Definition::class,
+                                $defaultFromDefinition
+                            );
+
+                            static::assertSame(
+                                [
+                                    'test@example.com',
+                                    'Test Email',
+                                ],
+                                $defaultFromDefinition->getArguments()
+                            );
+                        }
+                    ),
+                ]
+            ),
+        ];
+
+        yield 'css_inliner' => [
+            [
+                [
+                    'css_inliner' => true,
+                ],
+            ],
+            array_merge(
+                $defaultServices,
+                [
+                    new ServiceConfiguration(
+                        'draw.mailer.event_listener.email_css_inliner_listener',
+                        [
+                            EmailCssInlinerListener::class,
+                        ],
+                    ),
+                ]
+            ),
+        ];
+    }
+}
